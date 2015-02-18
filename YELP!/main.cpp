89159ae68db0479e12b46d9fc3c52529401a78a6
raymondborkowski <rborkows@umich.edu>
2015-02-18 12:30:55 -0500

    Initial Commit

---
+++ YELP!/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  YELP!
+//
+//  Created by Raymond Borkowski on 2/18/15.
+//  Copyright (c) 2015 Raymond Borkowski. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
