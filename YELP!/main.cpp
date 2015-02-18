19f0865531d42933f31815dbde1e336d4ed9a6fc
raymondborkowski <rborkows@umich.edu>
2015-02-18 13:08:08 -0500

    Submitted to YELP!

--- YELP!/main.cpp
+++ YELP!/main.cpp
@@ -5,11 +5,27 @@
 //  Created by Raymond Borkowski on 2/18/15.
 //  Copyright (c) 2015 Raymond Borkowski. All rights reserved.
 //
-
+//
 #include <iostream>
-
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!!\n";
+#include <string>
+#include <algorithm>
+using namespace std;
+//n strings from input
+//naive solution using sort Which is O(nlogn)
+int main() {
+    /* Enter your code here. Read input from STDIN. Print output to STDOUT */
+    string wordOne, wordTwo;
+    cin >> wordOne;
+    cin>> wordTwo;
+    
+    transform(wordOne.begin(), wordOne.end(), wordOne.begin(), ::tolower);
+    transform(wordTwo.begin(), wordTwo.end(), wordTwo.begin(), ::tolower);
+    
+    sort(wordOne.begin(),wordOne.end());
+    sort(wordTwo.begin(),wordTwo.end());
+    if(wordOne == wordTwo)
+        cout<<"true\n";
+    else
+        cout<<"false\n";
     return 0;
 }
